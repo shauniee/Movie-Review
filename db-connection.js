1c5f042a1987121eaa74271f6b53321bfb3f63d0
Shaun <67725026+shauniee@users.noreply.github.com>
2022-07-19 23:46:59 +0800

    Delete db-connection.js

--- db-connection.js
+++
@@ -1,9 +0,0 @@
-var mysql = require('mysql');
-var connection = mysql.createPool({
-    host:'localhost',
-    port:'3306',
-    user:'root',
-    password:'',
-    database:'movie_review'
-});
-module.exports = connection;
